ec0a4112fe0df35bbc374b1234670a276294609c
Cristian Camilo <camilogaravitod00@gmail.com>
2025-09-24 10:05:22 -0500

    Subiendo todo con los cambios de Mauricio al reposito

--- src/app.js
+++ src/app.js
@@ -1,33 +1,27 @@
 require('dotenv').config();
 const express = require('express');
 const userRoutes = require('./routes/user.routes');
-<<<<<<< HEAD
 
 const authRoutes = require('./routes/auth.routes');
 const orderRoutes = require('./routes/order.routes');
 
 // Inicializando 
-=======
 const inventiryRoutes = require('./routes/inventiry.routes');
 
->>>>>>> origin/oscar
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-<<<<<<< HEAD
 // Enrutando
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 
-=======
 app.use('/api/users', userRoutes);
 // app.use('/api/auth', authRoutes);
 // app.use('/api/products', productRoutes);
 app.use('/api/inventories', inventiryRoutes);
->>>>>>> origin/oscar
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
